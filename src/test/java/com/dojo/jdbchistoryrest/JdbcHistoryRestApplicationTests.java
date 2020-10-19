779d146526d415fb22bb36a2a86946d4602e32e6
yoshifumi <yoshifumi@kobain.local>
2020-10-19 18:28:43 +0900

    first commit

---
+++ src/test/java/com/dojo/jdbchistoryrest/JdbcHistoryRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dojo.jdbchistoryrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdbcHistoryRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
