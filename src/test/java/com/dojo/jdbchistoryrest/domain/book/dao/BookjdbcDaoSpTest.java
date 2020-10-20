10daebc4a0a044ff2c304aef2957141dc5174a92
yoshifumi <yoshifumi@kobain.local>
2020-10-20 17:14:35 +0900

    refactor code

--- src/test/java/com/dojo/jdbchistoryrest/domain/book/dao/BookjdbcDaoSpTest.java
+++ src/test/java/com/dojo/jdbchistoryrest/domain/book/dao/BookjdbcDaoSpTest.java
@@ -16,7 +16,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.JdbcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import com.dojo.jdbchistoryrest.domain.book.entity.Book;
