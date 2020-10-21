3baf06663f90e494ff7139f625362faa9deb22ca
yoshifumi <yoshifumi@kobain.local>
2020-10-21 16:30:05 +0900

    refactor code

--- src/test/java/com/dojo/jdbchistoryrest/domain/user/jpa/IUserJpaRepositoryTest.java
+++ src/test/java/com/dojo/jdbchistoryrest/domain/user/jpa/IUserJpaRepositoryTest.java
@@ -1,5 +1,10 @@
 package com.dojo.jdbchistoryrest.domain.user.jpa;
 
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.samePropertyValuesAs;
+import static org.junit.Assert.assertThat;
+
 import java.sql.Date;
 
 import org.junit.jupiter.api.Test;
@@ -38,5 +43,34 @@ public class IUserJpaRepositoryTest {
 		user1.setBirthDay(Date.valueOf("2001-01-01"));
 
 		//assertThat(it.findById(1L).orElse(null), is(samePropertyValuesAs(user1)));
+	}
+	
+	@Test
+	public void testDelete() {
+		//assertThat(it.deleteById(2L), is(1));
+		//assertThat(it.findById(2L).orElse(null), is(nullValue()));
+	}
+
+	@Test
+	public void testCreate() {
+		User user1 = new User();
+		user1.setUserId(3);
+		user1.setUserName("user3");
+		user1.setBirthDay(Date.valueOf("2003-03-03"));
+
+		//assertThat(it.create(user1), is(samePropertyValuesAs(user1));
+		//assertThat(it.findById(3L).orElse(null), is(samePropertyValuesAs(user1)));
+
+	}
+
+	@Test
+	public void testUpdate() {
+		User user1 = new User();
+		user1.setUserId(1);
+		user1.setUserName("user11");
+		user1.setBirthDay(Date.valueOf("2000-01-01"));
+		//assertThat(it.update(user1), is(samePropertyValuesAs(user1));
+		//assertThat(it.findById(1L).orElse(null), is(samePropertyValuesAs(user1)));
+
 	}
 }
