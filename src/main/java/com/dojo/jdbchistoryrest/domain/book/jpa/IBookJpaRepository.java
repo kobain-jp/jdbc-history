bf4c3e1cb50ba1a419f44329b1afb43ef3ffd36c
yoshifumi <yoshifumi@kobain.local>
2020-10-21 16:25:43 +0900

    refactor code

--- src/main/java/com/dojo/jdbchistoryrest/domain/book/jpa/IBookJpaRepository.java
+++ src/main/java/com/dojo/jdbchistoryrest/domain/book/jpa/IBookJpaRepository.java
@@ -1,9 +1,13 @@
 package com.dojo.jdbchistoryrest.domain.book.jpa;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.dojo.jdbchistoryrest.domain.book.entity.Book;
 
 public interface IBookJpaRepository extends JpaRepository<Book, Long> {
+
+	List<Book> findByTitleLike(String title);
 
 }
