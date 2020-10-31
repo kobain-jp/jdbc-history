04514369b773a2eb7ea88b16434bbbac10ce3383
yoshifumi <yoshifumi@kobain.local>
2020-10-31 21:10:04 +0900

    add vue form

--- src/main/resources/static/js/book/vue/app.js
+++ src/main/resources/static/js/book/vue/app.js
@@ -1,9 +1,39 @@
-new Vue({
-  el: '#app',
-  data: {
-    books: []
+const pageInput =Vue.component('page-input', {
+  data: function () {
+    return {
+      form: {
+        isbn: '',
+        title: '',
+        author: '',
+        releaseDate: '',
+      }
+    }
   }, methods: {
 
+    doCreateBook: function () {
+      axios.post('/api/book', this.form)
+        .then(response => router.push({ path: 'index' }) )
+        .catch(response => console.log(response));
+    }
+
+  },
+  template: '#page-input'
+})
+
+const pageIndex =Vue.component('page-index', {
+  data: function () {
+    return {
+      books: []
+    }
+  }, methods: {
+
+    fetch: function () {
+      axios.get('/api/book')
+        .then(response => this.books = response.data)
+        .catch(response => console.log(response));
+
+    }
+    ,
     searchByTitle: function () {
 
       let title = this.$refs.searchValue.value;
@@ -11,16 +41,28 @@ new Vue({
       axios.get('/api/book-title', { params: { title: title } })
         .then(response => this.books = response.data)
         .catch(response => console.log(response));
-
     }
 
   },
 
   mounted: function () {
+    this.fetch();
+  },
+  template: '#page-index'
+});
 
-    axios.get('/api/book')
-      .then(response => this.books = response.data)
-      .catch(response => console.log(response));
+const routes = [
+  { path: '/index', component: pageIndex },
+  { path: '/input', component: pageInput }
+]
 
-  }
+const router = new VueRouter({
+	routes: routes
 })
+
+new Vue({
+  el: '#app',
+  router: router
+});
+
+router.push({ path: 'index' })
